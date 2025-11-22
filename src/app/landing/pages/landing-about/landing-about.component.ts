488af925642033bc0c4aa0e3dd663dbfad27d66f
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2025-11-23 02:37:20 +0530

    Landing Code Updated

--- src/app/landing/pages/landing-about/landing-about.component.ts
+++ src/app/landing/pages/landing-about/landing-about.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonService } from '../../../shared/services/common.service';
 
 @Component({
   selector: 'app-landing-about',
   templateUrl: './landing-about.component.html',
   styleUrl: './landing-about.component.css'
 })
-export class LandingAboutComponent {
+export class LandingAboutComponent implements OnInit{
+  constructor(
+    private commonService: CommonService
+  ) { }
 
+  ngOnInit(): void {
+    
+  }
+
+  navigateToPage(path: string) {
+    this.commonService.navigateToPage(path, false);
+  }
 }
--- src/app/landing/pages/landing-home/landing-home.component.ts
+++ src/app/landing/pages/landing-home/landing-home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonService } from '../../../shared/services/common.service';
 
 @Component({
   selector: 'app-landing-home',
   templateUrl: './landing-home.component.html',
   styleUrl: './landing-home.component.css'
 })
-export class LandingHomeComponent {
+export class LandingHomeComponent implements OnInit{
+  menuOpen = false;
 
+  constructor(
+    private commonService: CommonService
+  ) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  navigateToPage(path: string) {
+    this.commonService.navigateToPage(path, false);
+  }
 }
