5ae8c03144484e426cc06ad8e5ab5b16e8926df6
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2025-11-29 03:26:31 +0530

    prod url fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'project-loadmaster-api.kussagrapathak.in',
-  socketUrl: 'project-loadmaster-api.kussagrapathak.in'
+  apiUrl: 'https://project-loadmaster-api.kussagrapathak.in',
+  socketUrl: 'https://project-loadmaster-api.kussagrapathak.in'
 };
